d68d608c7b9ef4e35d1b2b300d5eb6952e7af80c
Phil Haines <phil.haines92@gmail.com>
2021-04-30 12:47:25 -0600

    Added apollo client. Wraped all components in apollo component in App

---
+++ apollo.tsx
@@ -0,0 +1,7 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const URI = "http://localhost:4000/";
+export const client = new ApolloClient({
+	uri: URI,
+	cache: new InMemoryCache(),
+});
