4a7e6c75638a3a5975259a3f91f051c3ba2ecb3f
Phil Haines <phil.haines92@gmail.com>
2021-05-05 17:40:14 -0600

    Added search mutation. Can search users in the backend via email or name.

--- screens/AddUsersToListScreen.tsx
+++ screens/AddUsersToListScreen.tsx
@@ -2,8 +2,33 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, TextInput } from "react-native";
 import { Text, View } from "../components/Themed";
 
+import { useQuery, gql } from "@apollo/client";
+
+const GET_USERS = gql`
+	query getUsers($text: String) {
+		getUsers(input: { text: $text }) {
+			id
+			email
+			name
+		}
+	}
+`;
+
 export default function AddUsersToListScreen() {
 	const [userSearch, setUserSearch] = useState("");
+	const [userData, setUserData] = useState([]);
+	const { data: usersData } = useQuery(GET_USERS, {
+		variables: {
+			text: userSearch,
+		},
+	});
+
+	useEffect(() => {
+		if (usersData) {
+			setUserData(usersData);
+			console.log(userData);
+		}
+	}, [userSearch]);
 	return (
 		<View style={styles.container}>
 			<View style={styles.inputContainer}>
