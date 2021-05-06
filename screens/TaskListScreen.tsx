3cfdcad267418241654edb588af204903b278b9d
Phil Haines <phil.haines92@gmail.com>
2021-05-06 15:51:59 -0600

    added UI element to trigger create mutation

--- screens/TaskListScreen.tsx
+++ screens/TaskListScreen.tsx
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, FlatList, Alert, ActivityIndicator } from "react-native";
+import {
+	StyleSheet,
+	FlatList,
+	Alert,
+	ActivityIndicator,
+	Pressable,
+} from "react-native";
 import { Text, View } from "../components/Themed";
 import { useQuery, gql } from "@apollo/client";
+import { Ionicons } from "@expo/vector-icons";
 import TaskList from "../components/TaskList/TaskList";
 import { MY_TASK_LISTS_QUERY } from "../Apollo/Queries";
 
@@ -32,6 +39,9 @@ export default function TabTwoScreen() {
 				renderItem={({ item }) => <TaskList taskList={item} />}
 				style={{ width: "100%" }}
 			/>
+			<Pressable style={styles.addTaskListButton}>
+				<Ionicons name="pencil" size={32} color="white" />
+			</Pressable>
 		</View>
 	);
 }
@@ -40,6 +50,18 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		alignItems: "center",
+		justifyContent: "flex-end",
+	},
+	addTaskListButton: {
+		height: 70,
+		width: 70,
+		backgroundColor: "#77df79",
+		position: "absolute",
+		alignSelf: "flex-end",
+		bottom: 60,
+		right: 30,
+		borderRadius: 50,
 		justifyContent: "center",
+		alignItems: "center",
 	},
 });
